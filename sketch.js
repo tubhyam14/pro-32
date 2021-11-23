3824995938efa62d60bc1fee2ae1bb5995cd47d3
tubhyam14 <77627781+tubhyam14@users.noreply.github.com>
2021-11-23 11:23:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,13 +31,7 @@ function draw(){
     fill("black");
     textSize(30);
 
-    if(hour>=12){
-        text("Time : "+ hour%12 + " PM", 50,100);
-       }else if(hour==0){
-         text("Time : 12 AM",100,100);
-       }else{
-        text("Time : "+ hour%12 + " AM", 50,100);
-       }
+    
 
 }
 
